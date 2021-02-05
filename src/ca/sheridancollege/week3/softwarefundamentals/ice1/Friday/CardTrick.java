a214118ab197a498bfe0bb8e0ef7e5e34bf54c77
Jaime Tavares <32743102+tavaresjaime00@users.noreply.github.com>
2021-02-05 11:55:32 -0500

    Added output of card1 object via the Github repo online

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,11 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card1 = new Card();
+       // give card1 values
+       card1.setValue(2);
+       card1.setSuit("Clubs");
+       System.out.println("Card1 Suit: " + card1.getSuit());
+       System.out.println("Card1 Value: " + card1.getValue());
     }
     
 }
